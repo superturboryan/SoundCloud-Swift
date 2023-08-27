1d976778c7c1704db3446b65138fb559033e66a3
Ryan Forsyth <forsyth.r@gmail.com>
2023-08-27 15:18:50 +0100

    Change hours and minutes string format

--- Sources/SoundCloud/Helpers/Int.swift
+++ Sources/SoundCloud/Helpers/Int.swift
@@ -30,10 +30,11 @@ public extension Int {
     }
     
     var hoursAndMinutesStringFromSeconds: String {
-        let minutes = String(format: "%02d", ((self % 3600) / 60))
-        var result = minutes + "mins"
+        let minutesInt = (self % 3600) / 60
+        let minutes = String(format: "%02d", minutesInt)
+        var result = minutes + "min" + (minutesInt == 1 ? "" : "s")
         if self > 3600 {
-            let hours = String(format: "%02d", (self / 3600))
+            let hours = String(format: "%d", (self / 3600))
             result = hours + "hr " + result
         }
         return result
