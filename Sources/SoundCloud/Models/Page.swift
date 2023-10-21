d5f14556c963be6648756fe706d11210299a4ab5
Ryan Forsyth <forsyth.r@gmail.com>
2023-10-21 11:23:21 +0100

    Page: add playlist convenience init when items are tracks

--- Sources/SoundCloud/Models/Page.swift
+++ Sources/SoundCloud/Models/Page.swift
@@ -8,18 +8,30 @@
 public struct Page<ItemType: Decodable>: Decodable {
     public var items: [ItemType]
     public var nextPage: String?
-    
+}
+
+extension Page {
+    public var hasNextPage: Bool { nextPage != nil }
+
     public mutating func update(with next: Page<ItemType>) {
         items += next.items
         nextPage = next.nextPage
     }
     
-    enum CodingKeys: String, CodingKey {
+    internal enum CodingKeys: String, CodingKey {
         case items = "collection"
         case nextPage = "nextHref"
     }
 }
 
-extension Page {
-    public var hasNextPage: Bool { nextPage != nil }
+public extension Page where ItemType == Track {
+    func playlist(id: Int, title: String, user: User) -> Playlist {
+        Playlist(
+            id: id,
+            user: user,
+            title: title,
+            tracks: self.items,
+            nextPageUrl: self.nextPage
+        )
+    }
 }
