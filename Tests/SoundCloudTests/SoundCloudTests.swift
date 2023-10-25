3ab36b8afac58a9165154a28c6c0ff7dba469fe2
Ryan Forsyth <forsyth.r@gmail.com>
2023-10-25 15:18:14 +0100

    Tests: remove SC instance

--- Tests/SoundCloudTests/SoundCloudTests.swift
+++ Tests/SoundCloudTests/SoundCloudTests.swift
@@ -6,6 +6,6 @@ final class SoundCloudTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SoundCloud().text, "Hello, World!")
+        
     }
 }
