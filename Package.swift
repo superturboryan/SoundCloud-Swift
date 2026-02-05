dcdeeec1b2bbd3ac7c2173dfa6a4a8d70dc66ff7
Ryan F <forsyth.r@gmail.com>
2026-02-05 21:17:13 +0000

    Package: Set swift-tools-version 5.10
    - Add resources to target dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.11
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -32,6 +32,10 @@ let package = Package(
                     name: "KeychainSwift",
                     package: "keychain-swift"
                 ),
+            ],
+            resources: [
+                .process("Assets.xcassets"),
+                .process("Localizable.xcstrings")
             ],
             swiftSettings: [
                 .enableExperimentalFeature("StrictConcurrency")
